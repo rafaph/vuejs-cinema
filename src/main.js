94b464b6996ad1241dd020c12f69150a6d1a837e
Raphael Castro <rafaphcastro@gmail.com>
2018-04-13 17:32:27 -0300

    first classes

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import './style.scss';
+
+new Vue({
+    el: '#app',
+    components: {
+        'movie-list': {
+            template: '<div id="movie-list"><div v-for="movie in movies" class="movie">{{ movie.title }}</div></div>',
+            data() {
+                return {
+                    movies: [
+                        {
+                            title: 'Pulp Fiction'
+                        },
+                        {
+                            title: 'Home Alone'
+                        },
+                        {
+                            title: 'Austin Powers'
+                        }
+                    ]
+                };
+            }
+        },
+        'movie-filter': {
+            template: '<div id="movie-filter"><h2>Filter results</h2></div>'
+        }
+    }
+});
